836f87b4674c5c15f62514b7552b7f80ed64f15b
Denis Hartl <denis.hartl@icloud.com>
2023-06-21 22:46:35 +0200

    Add function to fetch list of all courses from CosmosDB

--- GetCourses/__init__.py
+++ GetCourses/__init__.py
@@ -1,8 +1,12 @@
 import logging
 
 import azure.functions as func
+import json
 
 
 def main(req: func.HttpRequest, courses: func.DocumentList) -> func.HttpResponse:
-    
-    return func.HttpResponse(courses.data[0].to_json())
+    course_list = []
+    for course in courses.data:
+        course_id = course.data['id']
+        course_list.append(course_id)
+    return func.HttpResponse(json.dumps(course_list))
