ce2cc63459a7f74f0c6d057fa52e458f458ec8b9
Denis Hartl <denis.hartl@icloud.com>
2023-06-27 22:48:38 +0200

    Running first version of the function

---
+++ CreateAttachment/__init__.py
@@ -0,0 +1,58 @@
+import base64
+import logging
+import os
+import uuid
+
+import azure.functions as func
+from azure.storage.blob import BlobServiceClient, BlobClient
+
+
+def main(req: func.HttpRequest, attachment: func.Out[func.Document]) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    try:
+        req_body = req.get_json()
+    except ValueError:
+        pass
+    else:
+        name = req_body.get('name')
+        ticket_id = req_body.get('ticket_id')
+        file = req_body.get('file')
+
+    if not name:
+        return func.HttpResponse(
+            'No name provided. Please pass a name in the body when calling this function.',
+            status_code=400
+        )
+    if not ticket_id:
+        return func.HttpResponse(
+            'No ticket ID provided. Please pass a ticket ID in the body when calling this function.',
+            status_code=400
+        )
+    if not file:
+        return func.HttpResponse(
+            'No file provided. Please pass a file in base64 encoded format in the body when calling this function.',
+            status_code=400
+        )
+    
+    attachment_file = base64.b64decode(file)
+    blob_connect_string = os.getenv('BLOB_CONNECT_STRING')
+    blob_container = os.getenv('BLOB_CONTAINER')
+    attachment_uuid = str(uuid.uuid1())
+
+    blob_service_client = BlobServiceClient.from_connection_string(blob_connect_string)
+    blob_client =blob_service_client.get_blob_client(container=blob_container,blob=attachment_uuid)
+    blob_client.upload_blob(attachment_file)
+    
+    attachment_json = {
+        'name': name,
+        'ticket_id': ticket_id,
+        'uuid': attachment_uuid,
+        'blob_link': "https://iuisef01b10e.blob.core.windows.net/attachment/" + attachment_uuid
+    }
+
+    attachment.set(func.Document.from_dict(attachment_json))
+    return func.HttpResponse(
+            status_code=200
+    )
+
