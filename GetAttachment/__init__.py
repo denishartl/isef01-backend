b9634dad3bcd7bf5152e25b80ed65ef7d8a76fcc
Denis Hartl <denis.hartl@icloud.com>
2023-06-27 23:43:49 +0200

    Add retries

--- GetAttachment/__init__.py
+++ GetAttachment/__init__.py
@@ -10,8 +10,15 @@ Expected query parameters:
 * id: ID of the ticket to return
 """
 
-def main(req: func.HttpRequest, attachment: func.DocumentList) -> func.HttpResponse:
+def main(req: func.HttpRequest, attachment: func.DocumentList, context: func.Context) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Current retry count: %s', context.retry_context.retry_count)
+
+
+    if context.retry_context.retry_count == context.retry_context.max_retry_count:
+        logging.warn(
+            f"Max retries of {context.retry_context.max_retry_count} for "
+            f"function {context.function_name} has been reached")
 
     attachment_param_id = req.params.get('id')
     if not attachment_param_id:
