51fbd8b82b90d4af28a92c338c2e9debe07b8a35
Paula <106445181+primzahlenpaula@users.noreply.github.com>
2023-07-08 15:30:43 +0200

    Output all ticket data

--- GetTickets/__init__.py
+++ GetTickets/__init__.py
@@ -16,9 +16,19 @@ def main(req: func.HttpRequest, tickets: func.DocumentList) -> func.HttpResponse
 # Returns a list of all tickets
     ticket_list = []
     for ticket in tickets:
-        ticket_id = ticket['id']
-        ticket_list.append(ticket_id)
-        return func.HttpResponse(
-            json.dumps(ticket_list),
-            status_code=200
-        )
+        ticket_data = {
+            'id': ticket ['id'],
+            'author_id': ticket ['author_id'],
+            'course_id': ticket ['course_id'],
+            'document_id': ticket ['document_id'],
+            'ticket_type': ticket ['ticket_type'],
+            'description': ticket ['description'],
+            'status': ticket ['status'],
+            'createdAt': ticket ['createdAt']
+    }
+        ticket_list.append(ticket_data)
+
+    return func.HttpResponse(
+        json.dumps(ticket_list),
+        status_code=200
+    )
