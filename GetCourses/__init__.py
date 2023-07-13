2e9cf0e5caec3bf8b7b301b35e663618ae7937e4
Paula <106445181+primzahlenpaula@users.noreply.github.com>
2023-07-13 20:06:34 +0200

    retrieval of attributes

--- GetCourses/__init__.py
+++ GetCourses/__init__.py
@@ -1,12 +1,22 @@
-import logging
-
 import azure.functions as func
 import json
 
 
 def main(req: func.HttpRequest, courses: func.DocumentList) -> func.HttpResponse:
+
+    # Returns a list of all tickets
     course_list = []
-    for course in courses.data:
-        course_id = course.data['id']
-        course_list.append(course_id)
-    return func.HttpResponse(json.dumps(course_list))
+    for course in courses:
+        course_data = {
+            'id': course ['id'],
+            'name': course ['name'],
+            'shortname': course ['shortname'],
+    }
+        course_list.append(course_data)
+
+    
+        
+    return func.HttpResponse(
+        json.dumps(course_list),
+        status_code=200
+        )
