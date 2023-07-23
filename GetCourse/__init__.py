7f5331e9a15eab05ea8b231dee6606e6ad7c8e95
Paula <106445181+primzahlenpaula@users.noreply.github.com>
2023-07-23 11:34:57 +0200

    added status codes

--- GetCourse/__init__.py
+++ GetCourse/__init__.py
@@ -1,19 +1,42 @@
 import azure.functions as func
 import json
+import logging
 
 
 def main(req: func.HttpRequest, course: func.DocumentList) -> func.HttpResponse:
-    course_param_id = req.params.get('id')
-    
-# Get course from CosmosDB via ticket_id
-    course_doc = {
-        'id': course[0]['id'],
-        'shortname': course[0]['shortname'],
-        'name': course[0]['name']    
-    }
+    logging.info('Python HTTP trigger function processed a request.')
 
-    # Course output
-    return func.HttpResponse(
-        json.dumps(course_doc),
-        status_code=200
-    )
+    course_id = req.params.get('id')
+    if not course_id:
+        return func.HttpResponse(
+            "Please insert course ID.",
+            status_code=400
+            )
+    
+    if not course:
+        return func.HttpResponse(
+             f"Could not find a course with the ID {course_id}.",
+              status_code=400
+              )
+    
+    else:
+        try:
+            # Get course from CosmosDB via ticket_id
+                course_doc = {
+                    'id': course[0]['id'],
+                    'shortname': course[0]['shortname'],
+                    'name': course[0]['name']    
+                }
+
+                # Returns the course data as http response
+                return func.HttpResponse(
+                    json.dumps(course_doc),
+                    status_code=200
+                )
+        
+        except Exception as ex:
+            logging.error(ex)
+            return func.HttpResponse(
+                "Error finding course ID.",
+                status_code=500
+                )
