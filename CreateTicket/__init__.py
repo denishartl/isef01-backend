e6d436bbc308cadd2da28e75d2410bc562cbfa35
Paula <106445181+primzahlenpaula@users.noreply.github.com>
2023-08-03 17:39:29 +0200

    bugfix

--- CreateTicket/__init__.py
+++ CreateTicket/__init__.py
@@ -32,7 +32,49 @@ def main(req: func.HttpRequest, ticket: func.Out[func.Document]) -> func.HttpRes
             status_code=400)
     
     try:
-        # Save Ticket in CosmosDB 
+        req_body = req.get_json()
+    except ValueError:
+        return func.HttpResponse(
+            'Invalid JSON payload',
+            status_code=400
+    )
+     
+    else:
+        author_id = req_body.get('author_id')
+        course_id = req_body.get('course_id')
+        document_id = req_body.get('document_id')
+        ticket_type = req_body.get('ticket_type')
+        description = req_body.get('description')
+    
+    # Return HTTP errors if something of the body is missing
+    try:
+        if not author_id:
+             return func.HttpResponse(
+                  'No author ID provided. Please enter author ID when using this function.',
+                  status_code=400
+             )
+        if not course_id:
+             return func.HttpResponse(
+                  'No course ID provided. Please enter course ID when using this function.',
+                  status_code=400
+             )
+        if not document_id:
+             return func.HttpResponse(
+                  'No document ID provided. Please enter document ID when using this function.',
+                  status_code=400
+             )
+        if not ticket_type:
+             return func.HttpResponse(
+                  'No ticket type  provided. Please enter ticket type when using this function.',
+                  status_code=400
+             )
+        if not description:
+             return func.HttpResponse(
+                  'No description provided. Please enter a description when using this function.',
+                  status_code=400
+             )
+
+        # Save information in CosmosDB 
         ticket_doc = {
             'author_id': author_id,
             'course_id': course_id,
@@ -43,15 +85,13 @@ def main(req: func.HttpRequest, ticket: func.Out[func.Document]) -> func.HttpRes
             'createdAt': datetime.datetime.utcnow().isoformat()
         }
 
-        # Save information to Azure CosmosDB
         ticket.set(func.Document.from_dict(ticket_doc))
-
         return func.HttpResponse(
             "Ticket created successfully.", 
-            status_code=200)
-
+            status_code=200
+        )
     except Exception as ex:
-        logging.error(ex)
-        return func.HttpResponse(
-            f"Error creating ticket: {str(ex)}", 
-            status_code=500)
+            logging.error(ex)
+            return func.HttpResponse(
+                f"Error creating ticket: {str(ex)}", 
+                status_code=500)
