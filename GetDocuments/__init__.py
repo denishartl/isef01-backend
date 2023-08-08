488a7e4192db92205102639125635cf4e1aaefc2
nicolescherliess <132930684+nicolescherliess@users.noreply.github.com>
2023-08-08 20:23:52 +0200

    Fehlerbehebung

--- GetDocuments/__init__.py
+++ GetDocuments/__init__.py
@@ -2,7 +2,6 @@ import json
 import logging
 import azure.functions as func
 
-
 def main(req: func.HttpRequest, documents: func.DocumentList, context: func.Context) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     logging.info('Current retry count: %s', context.retry_context.max_retry_count)
@@ -12,16 +11,16 @@ def main(req: func.HttpRequest, documents: func.DocumentList, context: func.Cont
         document_list = []
         for document in documents:
             document_data = {
-                'id': document[0]['id'],
-                'title': document[0]['title'],
-                'doctype': document[0]['doctype'],
-        }
+                'id': document['id'],
+                'title': document['title'],
+                'doctype': document['doctype'],
+            }
             document_list.append(document_data)
             
         return func.HttpResponse(
             json.dumps(document_list),
             status_code=200
-            )
+        )
     
     except Exception as ex:
         logging.error(ex)
