db736f6cc586ec56049f231b087f4c4832bd71b7
Denis Hartl <denis.hartl@icloud.com>
2023-08-17 20:26:33 +0200

    Finish tests for GetDocument

---
+++ test/GetDocument_test.py
@@ -0,0 +1,84 @@
+import unittest
+import azure.functions as func
+import json
+
+from GetDocument import main # import the method we want to test
+
+document_id = '32214-fg342-fds856fasd-4fdsaf5sa'
+document_title = 'ISEF01 - Skript'
+doctype = 'Skript'
+
+
+class TestGetDocument(unittest.TestCase):
+    def test_get_document_correct(self):
+        request = func.HttpRequest(
+            method='GET',
+            url='/api/GetDocument',
+            params={'id': document_id},
+            body=None
+        )
+
+        document = func.DocumentList(
+            [
+                func.Document(
+                    {
+                        'id': document_id,
+                        'title': document_title,
+                        'doctype': doctype
+                    }
+                )
+            ]
+        )
+
+        # Act
+        response = main(request, document)
+
+        # Assert
+        # Assert status code
+        assert response.status_code == 200
+
+        # Assert the response is as expected
+        response_json = json.loads(response.get_body())
+        assert response_json['id'] == document_id
+        assert response_json['title'] == document_title
+        assert response_json['doctype'] == doctype
+
+
+    def test_get_document_noparam(self):
+        request = func.HttpRequest(
+            method='GET',
+            url='/api/GetDocument',
+            params={},
+            body=None
+        )
+
+        document = func.DocumentList(initlist=None)
+
+        # Act
+        response = main(request,document)
+
+        # Assert status code
+        assert response.status_code == 400
+
+        # Assert the response is as expected
+        assert 'Please choose a document ID.' in response.get_body().decode()
+
+
+    def test_get_document_unknowncourse(self):
+        request = func.HttpRequest(
+            method='GET',
+            url='/api/GetDocument',
+            params={'id': document_id},
+            body=None
+        )
+
+        document = func.DocumentList(initlist=None)
+
+        # Act
+        response = main(request,document)
+
+        # Assert status code
+        assert response.status_code == 400
+
+        # Assert the response is as expected
+        assert f'Could not find document with ID {document_id}.' in response.get_body().decode()
