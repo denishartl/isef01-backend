65f9f3de1ab3478abf60f57cda7a8202909517c8
Denis Hartl <denis.hartl@icloud.com>
2023-09-26 13:41:08 +0200

    Add check for user role

--- GetTicketsByUserID/__init__.py
+++ GetTicketsByUserID/__init__.py
@@ -1,27 +1,38 @@
 import azure.functions as func
 import json
 import logging
+import requests
 
 """
 
-Returns a list of tickets based on the user ID provided
+Returns a list of tickets based on the user ID provided.
+If user is role "Bearbeiter", return all tickets.
+If user is role "User", only return user's own tickets
 
 """
 
-def main(req: func.HttpRequest, tickets: func.DocumentList) -> func.HttpResponse:
+def main(req: func.HttpRequest, usertickets: func.DocumentList, alltickets: func.DocumentList) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     
-    course_id = req.params.get('user_id')
-    if not course_id:
+    user_id = req.params.get('user_id')
+    if not user_id:
         return func.HttpResponse(
             "Please provide a user ID.",
             status_code=400
         )
 
+    user_response = requests.get(f'https://iu-isef01-functionapp.azurewebsites.net/api/getuser?user_id={user_id}')
+    user_response_json = json.loads(user_response.content)
+
+    if user_response_json['role'] == 'Bearbeiter':
+        tickets_to_return = alltickets
+    elif user_response_json['role'] == 'User':
+         tickets_to_return = usertickets
+
     try:
             # Returns a list of all tickets
                 ticket_list = []
-                for ticket in tickets:
+                for ticket in tickets_to_return:
                     ticket_data = {
                         'id': ticket ['id'],
                         'author_id': ticket ['author_id'],
