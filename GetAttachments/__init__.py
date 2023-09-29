1cd6f752309be8f74640096f759eba0e1c665f31
Denis Hartl <denis.hartl@icloud.com>
2023-09-29 20:47:48 +0200

    Always return attachment_list, even if empty (always return JSON)

--- GetAttachments/__init__.py
+++ GetAttachments/__init__.py
@@ -20,15 +20,15 @@ def main(req: func.HttpRequest, attachments: func.DocumentList) -> func.HttpResp
             "Please provide a ticket ID to query for.",
             status_code=400
         )
-
+    
+    attachment_list = []
     if not attachments:
         return func.HttpResponse(
-            f"Could not find any attachment for a ticket with ID {ticket_id}.",
-            status_code=400
+            json.dumps(attachment_list),
+            status_code=200
         )
     else:
         try:
-            attachment_list = []
             for attachment in attachments.data:
                 attachment_list.append(attachment.data)
             return func.HttpResponse(
