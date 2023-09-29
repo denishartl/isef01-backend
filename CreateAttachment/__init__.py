f8efc1477deeb843b3657eab7292c8d6d7414887
Denis Hartl <denis.hartl@icloud.com>
2023-09-29 09:38:43 +0200

    Add content disposition header when uploading blob

--- CreateAttachment/__init__.py
+++ CreateAttachment/__init__.py
@@ -5,7 +5,7 @@ import requests
 import uuid
 
 import azure.functions as func
-from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import BlobServiceClient, ContentSettings
 
 """
 This function uploads a file to Azure Blob Storage and saves it's link along with metadata to Azure CosmosDB:
@@ -71,9 +71,12 @@ def main(req: func.HttpRequest, attachment: func.Out[func.Document]) -> func.Htt
         blob_container = os.getenv('BLOB_CONTAINER')
         attachment_uuid = str(uuid.uuid1())
 
+
         blob_service_client = BlobServiceClient.from_connection_string(blob_connect_string)
-        blob_client =blob_service_client.get_blob_client(container=blob_container,blob=attachment_uuid)
+        blob_client = blob_service_client.get_blob_client(container=blob_container,blob=attachment_uuid)
         blob_client.upload_blob(attachment_file)
+        blob_headers = ContentSettings(content_disposition=f"attachment; filename={name}")
+        blob_client.set_http_headers(blob_headers)
         
         # Save information to Azure CosmosDB
         attachment_json = {
